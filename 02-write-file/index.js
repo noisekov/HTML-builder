c978b5e8ebc0f19e02650c931c2848bda1332c66
Vladimir <noise123q@mail.ru>
2023-04-26 20:33:30 +0300

    feat: solve 03-files-in-folder

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -4,13 +4,23 @@ const fs = require("fs");
 const file = path.join(__dirname, "yourText.txt");
 
 let readableStream = fs.createWriteStream(file, "utf-8");
-console.log(`Введите ваше сообщение:`);
+console.log("Введите ваше сообщение:");
+
+const readline = require("readline");
+const { stdin: input, stdout: output } = require("process");
+const rl = readline.createInterface({ input, output });
+
+rl.on("SIGINT", () => {
+  console.log("До встречи!");
+  rl.close();
+});
 
 process.stdin.on("data", data => {
-    readableStream.write(`${data.toString()}`);
+  readableStream.write(`${data.toString()}`);
 
-    if (`${data.toString()}`.trim() == "exit") {
-        console.log(`До встречи!`);
-        process.exit();
-    }
+  if (`${data.toString()}`.trim() == "exit") {
+    console.log(`До встречи!`);
+    process.exit();
+  }
+
 });
---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const path = require("node:path");
+
+const pathToFolder = path.join(__dirname, "secret-folder");
+
+fs.readdir(pathToFolder, {withFileTypes: true}, (_, files) => {
+  for (let prop of files) {
+    if (!prop.isDirectory()) {
+      const nameFile = prop.name.split(".")[0];
+      const extnameFile = path.extname(prop.name).replace(".", "");
+
+      fs.stat(path.join(__dirname, `/secret-folder/${prop.name}`), (_, stats) => {
+          console.log(`${nameFile} - ${extnameFile} - ${(stats.size / 1024).toFixed(3)}kb`);
+      });
+    }
+  }
+})
